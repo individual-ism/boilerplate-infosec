e52dbfa01c42f607cb5a6b0cf905b5c55719add1
individual-ism <individual-ism-code@proton.me>
2023-03-15 14:00:31 -0400

    Mounts frameguard()

--- myApp.js
+++ myApp.js
@@ -2,10 +2,9 @@ const express = require('express')
 const app = express();
 
 const helmet = require('helmet')
+
 app.use(helmet.hidePoweredBy())
-
-
-
+app.use(helmet.frameguard({action: 'deny'}))
 
 
 

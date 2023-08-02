3a1be062ec4231bc49baad6adfda67abd7bac33f
individual-ism <individual-ism-code@proton.me>
2023-08-01 23:42:54 -0400

    FCC 2

--- myApp.js
+++ myApp.js
@@ -5,14 +5,14 @@ const app = express();
 
 const ninetyDaysInSeconds = 90 * 24 * 60 * 60
 
-// app.use(helmet.hidePoweredBy())
+app.use(helmet.hidePoweredBy())
 // app.use(helmet.frameguard({action: 'deny'}))
 // app.use(helmet.xssFilter())
 // app.use(helmet.noSniff())
 // app.use(helmet.ieNoOpen())
 // app.use(helmet.hsts({maxAge: ninetyDaysInSeconds, force: true}))
 // app.use(helmet.dnsPrefetchControl())
-app.use(helmet.noCache())
+// app.use(helmet.noCache())
 // app.use(helmet.contentSecurityPolicy({
 //     directives: {
 //         defaultSrc: ["'self'"],
@@ -20,21 +20,21 @@ app.use(helmet.noCache())
 //     }
 // }))
 
-app.use(helmet({
-    frameguard: {
-        action: 'deny'
-    },
-    hsts: {
-        maxAge: ninetyDaysInSeconds,
-        force: true
-    },
-    contentSecurityPolicy: {
-        directives: {
-            defaultSrc: ["'self'"],
-            scriptSrc: ["'self'", 'trusted-cdn.com']
-        }
-    }
-}))
+// app.use(helmet({
+//     frameguard: {
+//         action: 'deny'
+//     },
+//     hsts: {
+//         maxAge: ninetyDaysInSeconds,
+//         force: true
+//     },
+//     contentSecurityPolicy: {
+//         directives: {
+//             defaultSrc: ["'self'"],
+//             scriptSrc: ["'self'", 'trusted-cdn.com']
+//         }
+//     }
+// }))
 
 
 

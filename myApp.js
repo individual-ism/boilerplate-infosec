e86afdfce28aaed03f8352763a6fffffaa888ec5
individual-ism <individual-ism-code@proton.me>
2023-08-01 23:53:12 -0400

    Revert "FCC3"
    This reverts commit 2d9a237d744225db450d9a234499c391b7e33293.

--- myApp.js
+++ myApp.js
@@ -6,7 +6,7 @@ const app = express();
 const ninetyDaysInSeconds = 90 * 24 * 60 * 60
 
 app.use(helmet.hidePoweredBy())
-// app.use(helmet.frameguard({action: 'deny'}))
+app.use(helmet.frameguard({action: 'deny'}))
 // app.use(helmet.xssFilter())
 // app.use(helmet.noSniff())
 // app.use(helmet.ieNoOpen())
@@ -20,21 +20,21 @@ app.use(helmet.hidePoweredBy())
 //     }
 // }))
 
-app.use(helmet({
-    frameguard: {
-        action: 'deny'
-    },
-    hsts: {
-        maxAge: ninetyDaysInSeconds,
-        force: true
-    },
-    contentSecurityPolicy: {
-        directives: {
-            defaultSrc: ["'self'"],
-            scriptSrc: ["'self'", 'trusted-cdn.com']
-        }
-    }
-}))
+// app.use(helmet({
+//     frameguard: {
+//         action: 'deny'
+//     },
+//     hsts: {
+//         maxAge: ninetyDaysInSeconds,
+//         force: true
+//     },
+//     contentSecurityPolicy: {
+//         directives: {
+//             defaultSrc: ["'self'"],
+//             scriptSrc: ["'self'", 'trusted-cdn.com']
+//         }
+//     }
+// }))
 
 
 

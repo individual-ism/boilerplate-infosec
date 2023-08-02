294659566a835fdbc2cbb9d6f2c5ee8999de049b
individual-ism <individual-ism-code@proton.me>
2023-08-01 23:56:49 -0400

    FCC 5

--- myApp.js
+++ myApp.js
@@ -8,7 +8,7 @@ const ninetyDaysInSeconds = 90 * 24 * 60 * 60
 app.use(helmet.hidePoweredBy())
 app.use(helmet.frameguard({action: 'deny'}))
 app.use(helmet.xssFilter())
-// app.use(helmet.noSniff())
+app.use(helmet.noSniff())
 // app.use(helmet.ieNoOpen())
 // app.use(helmet.hsts({maxAge: ninetyDaysInSeconds, force: true}))
 // app.use(helmet.dnsPrefetchControl())
